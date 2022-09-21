be3b74547d8e41b9cab68ddfb1e132c51c21e18b
skylightis666 <skylightis666@gmail.com>
2022-09-21 15:10:08 +0300

    Fixes for PR

--- src/test/java/hw5/steps/ActionStep.java
+++ src/test/java/hw5/steps/ActionStep.java
@@ -10,23 +10,23 @@ public class ActionStep extends AbstractStep {
         homePage.getHeaderElements().clickHeaderItem(item);
     }
 
-    @And("I click on {string} button in Service dropdown")
+    @When("I click on {string} button in Service dropdown")
     public void openDropdownItem(String item) {
         homePage.getHeaderElements().clickHeaderItem("SERVICE");
         homePage.clickDropdownMenu(item);
     }
 
-    @And("I select {string} checkboxes in main form on Different Elements page")
+    @When("I select {string} checkboxes in main form on Different Elements page")
     public void clickCheckboxDiffEl(String checkbox) {
         diffEl.clickCheckBox(checkbox);
     }
 
-    @And("I select radio buttons {string} in main form on Different Elements page")
+    @When("I select radio buttons {string} in main form on Different Elements page")
     public void clickRadioboxDiffEl(String radiobox) {
         diffEl.clickRadioBox(radiobox);
     }
 
-    @And("I select {string} in dropdown in main form on Different Elements page")
+    @When("I select {string} in dropdown in main form on Different Elements page")
     public void clickDropdownDiffEl(String dropdown) {
         diffEl.clickOption(dropdown);
     }
